a2e126c256ec643828a4b3f9b342a2f8c66a7e4f
Soraxe <vanbx@outlook.fr>
2026-09-11 11:55:31 +0200

    Test Tower

--- PFEjeu/Assets/Scripts/TowerSystem/Tower.cs
+++ PFEjeu/Assets/Scripts/TowerSystem/Tower.cs
@@ -3,5 +3,18 @@ using UnityEngine;
 
 public class Tower : MonoBehaviour
 {
-
+    [SerializeField] private GameObject level1;
+    [SerializeField] private GameObject level2;
+    [SerializeField] private GameObject level3;
+    
+    // Zone de tire
+    private void OnTriggerEnter(Collider other)
+    {
+        
+    }
+    
+    private void OnTrigggerExit(Collider other)
+    {
+        
+    }
 }
