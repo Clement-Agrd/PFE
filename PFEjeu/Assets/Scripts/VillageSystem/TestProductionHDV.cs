8aa98967d2963fcfab112de01c774b757dfc96a7
Maxence Lemaitre <maxencelemaitre028@gmail.com>
2026-09-22 11:57:35 +0200

    début village

---
+++ PFEjeu/Assets/Scripts/VillageSystem/TestProductionHDV.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using Core.InventorySystem;
+
+public class TestProductionHDV : MonoBehaviour
+{
+    [SerializeField] private InventoryHolder hdvStorage;
+    [SerializeField] private ItemDefinition bois;
+    [SerializeField] private ItemDefinition pierre;
+    [SerializeField] private ItemDefinition fer;
+    [SerializeField] private ItemDefinition nourriture;
+
+    private float timer;
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer < 2f) return;
+        timer = 0f;
+
+        var inv = hdvStorage.Inventory;
+        Debug.Log($"[HDV] Bois={inv.Count(bois)} Pierre={inv.Count(pierre)} Fer={inv.Count(fer)} Nourriture={inv.Count(nourriture)}");
+    }
+}
